7e5c81808afe4b409dea95cd942b7dcc19ef401f
JaniKononen <bhm556@myy.haaga-helia.fi>
2024-05-07 21:02:11 +0300

    lisätty_kuvat_ja_joonan_muutokset

--- functiot.js
+++ functiot.js
@@ -9,8 +9,8 @@ document.body.appendChild(kuvaAlue);
 
 //määritellään kuva
 let kuva = new Image();
-kuva.src = "Kuvat/linnanmaki.jpg"; //kuvan lähde
-kuva.alt = "Linnanmäki"; //vaihtoehtoinen teksti
+kuva.src = "Kuvat/karuselli.png"; //kuvan lähde
+kuva.alt = "karuselli"; //vaihtoehtoinen teksti
 
 // Lisää kuvaelementti kuva-alue-diviin
 kuvaAlue.appendChild(kuva);
@@ -30,103 +30,284 @@ let sisältöteksti = "";
         
                 // Luo uusi kuvaelementti
 
-                uusiKuva.src = "Kuvat/nakit&muusi.jpg"; // Aseta uuden kuvan lähde
+                uusiKuva.src = "Kuvat/linnanmaki.jpg"; // Aseta uuden kuvan lähde
         
                 // Lisää kuva-alueeseen
                 document.getElementById("kuva-alue").appendChild(uusiKuva);
         
                 //otsikko- ja sisältötekstit
-                document.getElementById("Otsikkoteksti").innerHTML = "Nakit ja muusi";
-                document.getElementById("Sisältöteksti").innerHTML = "Tuomas Jaakkola Lauantai-aamuna klo 1 matkalla kotia.";
+                document.getElementById("Otsikkoteksti").innerHTML = "Linnanmäki";
+                document.getElementById("Sisältöteksti").innerHTML = "Linnanmäki on Suomen suosituin ja vanhin huvipuisto. Tämä 74-vuotias Alppilan ylpeys tarjoaa monipuolisen päivän koko perheelle. Yli 50 laitetta ja peliä takaavat uusia elämyksiä jokaiselle. Suositus ";
+                
                 
                 // näyttää ajankohtaisen luukun kun painettu
                 document.getElementById("btn1").style.backgroundColor = "green";
             } else {
-                window.alert("Ei toimi!");
+                window.alert("Väärä viikko Donkey!");
             }
         }
         
         function kalenteriLuukku2(){
-            if(paiva >= 27 && kuukausi >= 5){
-            document.getElementById("teksti").innerHTML = "Hyvä";
+            if(paiva >= 7 && kuukausi >= 5){
+            // Poista aiempi kuva-alueen sisältö
+            document.getElementById("kuva-alue").innerHTML = "";
+        
+            // Luo uusi kuvaelementti
+
+            uusiKuva.src = "Kuvat/korkeasaari.jpg"; // Aseta uuden kuvan lähde
+    
+            // Lisää kuva-alueeseen
+            document.getElementById("kuva-alue").appendChild(uusiKuva);
+    
+            //otsikko- ja sisältötekstit
+            document.getElementById("Otsikkoteksti").innerHTML = "Korkeasaari";
+            document.getElementById("Sisältöteksti").innerHTML = "Korkeasaari on pakollinen käyntikohde jokaiselle eläinten ystävälle. Eläintarhasta löytyy noin 150 eri eläinlajia kotimaan tutuista metsäneläimistä trooppisiin erikoisuuksiin.";
+            
+            // näyttää ajankohtaisen luukun kun painettu
+            document.getElementById("btn2").style.backgroundColor = "green";
             }else{
-            window.alert("Ei toimi!")
+            window.alert("Väärä viikko Donkey!")
             }
         }
         function kalenteriLuukku3(){
             if(paiva >= 3 && kuukausi >= 6){
-            document.getElementById("teksti").innerHTML = "Hyvä";
+            // Poista aiempi kuva-alueen sisältö
+            document.getElementById("kuva-alue").innerHTML = "";
+        
+            // Luo uusi kuvaelementti
+
+            uusiKuva.src = "Kuvat/Helsinki-Cup_logo.jpg.jpg"; // Aseta uuden kuvan lähde
+    
+            // Lisää kuva-alueeseen
+            document.getElementById("kuva-alue").appendChild(uusiKuva);
+    
+            //otsikko- ja sisältötekstit
+            document.getElementById("Otsikkoteksti").innerHTML = "Helsinki-Cup";
+            document.getElementById("Sisältöteksti").innerHTML = "Helsinki-cup on kansainvälisestikin merkittävä juniorijalkapalloturnaus, sillä siinä pelaa yli 1800 joukkuetta jopa 20 maasta. Turnauksella on pitkät perinteet, sillä se on pelattu vuosittain jo vuodesta 1976. Helsinki-cup ei ole vain jalkapalloturnaus, vaan siihen kuuluu myös tapahtumatori, avajaiskulkue ja kisadisco.";
+            
+            // näyttää ajankohtaisen luukun kun painettu
+            document.getElementById("btn3").style.backgroundColor = "green";
             }else{
-            window.alert("Ei toimi!")
+            window.alert("Väärä viikko Donkey!")
             }
         }
         function kalenteriLuukku4(){
             if(paiva >= 10 && kuukausi >= 6){
-            document.getElementById("teksti").innerHTML = "Hyvä";
+            // Poista aiempi kuva-alueen sisältö
+            document.getElementById("kuva-alue").innerHTML = "";
+        
+            // Luo uusi kuvaelementti
+
+            uusiKuva.src = ""; // Aseta uuden kuvan lähde
+    
+            // Lisää kuva-alueeseen
+            document.getElementById("kuva-alue").appendChild(uusiKuva);
+    
+            //otsikko- ja sisältötekstit
+            document.getElementById("Otsikkoteksti").innerHTML = "";
+            document.getElementById("Sisältöteksti").innerHTML = "";
+            
+            // näyttää ajankohtaisen luukun kun painettu
+            document.getElementById("btn1").style.backgroundColor = "green";
             }else{
-            window.alert("Ei toimi!")
+            window.alert("Väärä viikko Donkey!")
             }
         }
         function kalenteriLuukku5(){
             if(paiva >= 17 && kuukausi >= 6){
-            document.getElementById("teksti").innerHTML = "Hyvä";
+            // Poista aiempi kuva-alueen sisältö
+            document.getElementById("kuva-alue").innerHTML = "";
+        
+            // Luo uusi kuvaelementti
+
+            uusiKuva.src = ""; // Aseta uuden kuvan lähde
+    
+            // Lisää kuva-alueeseen
+            document.getElementById("kuva-alue").appendChild(uusiKuva);
+    
+            //otsikko- ja sisältötekstit
+            document.getElementById("Otsikkoteksti").innerHTML = "";
+            document.getElementById("Sisältöteksti").innerHTML = "";
+            
+            // näyttää ajankohtaisen luukun kun painettu
+            document.getElementById("btn1").style.backgroundColor = "green";
             }else{
-            window.alert("Ei toimi!")
+            window.alert("Väärä viikko Donkey!")
             }
         }
         function kalenteriLuukku6(){
             if(paiva >= 24 && kuukausi >= 6){
-            document.getElementById("teksti").innerHTML = "Hyvä";
+            // Poista aiempi kuva-alueen sisältö
+            document.getElementById("kuva-alue").innerHTML = "";
+        
+            // Luo uusi kuvaelementti
+
+            uusiKuva.src = ""; // Aseta uuden kuvan lähde
+    
+            // Lisää kuva-alueeseen
+            document.getElementById("kuva-alue").appendChild(uusiKuva);
+    
+            //otsikko- ja sisältötekstit
+            document.getElementById("Otsikkoteksti").innerHTML = "";
+            document.getElementById("Sisältöteksti").innerHTML = "";
+            
+            // näyttää ajankohtaisen luukun kun painettu
+            document.getElementById("btn1").style.backgroundColor = "green";
             }else{
-            window.alert("Ei toimi!")
+            window.alert("Väärä viikko Donkey!")
             }
         }
         function kalenteriLuukku7(){
             if(paiva >= 1 && kuukausi >= 7){
-            document.getElementById("teksti").innerHTML = "Hyvä";
+            // Poista aiempi kuva-alueen sisältö
+            document.getElementById("kuva-alue").innerHTML = "";
+        
+            // Luo uusi kuvaelementti
+
+            uusiKuva.src = ""; // Aseta uuden kuvan lähde
+    
+            // Lisää kuva-alueeseen
+            document.getElementById("kuva-alue").appendChild(uusiKuva);
+    
+            //otsikko- ja sisältötekstit
+            document.getElementById("Otsikkoteksti").innerHTML = "";
+            document.getElementById("Sisältöteksti").innerHTML = "";
+            
+            // näyttää ajankohtaisen luukun kun painettu
+            document.getElementById("btn1").style.backgroundColor = "green";
             }else{
-            window.alert("Ei toimi!")
+            window.alert("Väärä viikko Donkey!")
             }
         }
         function kalenteriLuukku8(){
             if(paiva >= 8 && kuukausi >= 7){
-            document.getElementById("teksti").innerHTML = "Hyvä";
+            // Poista aiempi kuva-alueen sisältö
+            document.getElementById("kuva-alue").innerHTML = "";
+        
+            // Luo uusi kuvaelementti
+
+            uusiKuva.src = ""; // Aseta uuden kuvan lähde
+    
+            // Lisää kuva-alueeseen
+            document.getElementById("kuva-alue").appendChild(uusiKuva);
+    
+            //otsikko- ja sisältötekstit
+            document.getElementById("Otsikkoteksti").innerHTML = "";
+            document.getElementById("Sisältöteksti").innerHTML = "";
+            
+            // näyttää ajankohtaisen luukun kun painettu
+            document.getElementById("btn1").style.backgroundColor = "green";
             }else{
-            window.alert("Ei toimi!")
+            window.alert("Väärä viikko Donkey!")
             }
         }
         function kalenteriLuukku9(){
             if(paiva >= 15 && kuukausi >= 7){
-            document.getElementById("teksti").innerHTML = "Hyvä";
+            // Poista aiempi kuva-alueen sisältö
+            document.getElementById("kuva-alue").innerHTML = "";
+        
+            // Luo uusi kuvaelementti
+
+            uusiKuva.src = ""; // Aseta uuden kuvan lähde
+    
+            // Lisää kuva-alueeseen
+            document.getElementById("kuva-alue").appendChild(uusiKuva);
+    
+            //otsikko- ja sisältötekstit
+            document.getElementById("Otsikkoteksti").innerHTML = "";
+            document.getElementById("Sisältöteksti").innerHTML = "";
+            
+            // näyttää ajankohtaisen luukun kun painettu
+            document.getElementById("btn1").style.backgroundColor = "green";
             }else{
-            window.alert("Ei toimi!")
+            window.alert("Väärä viikko Donkey!")
             }
         }
         function kalenteriLuukku10(){
             if(paiva >= 22 && kuukausi >= 7){
-            document.getElementById("teksti").innerHTML = "Hyvä";
+            // Poista aiempi kuva-alueen sisältö
+            document.getElementById("kuva-alue").innerHTML = "";
+        
+            // Luo uusi kuvaelementti
+
+            uusiKuva.src = ""; // Aseta uuden kuvan lähde
+    
+            // Lisää kuva-alueeseen
+            document.getElementById("kuva-alue").appendChild(uusiKuva);
+    
+            //otsikko- ja sisältötekstit
+            document.getElementById("Otsikkoteksti").innerHTML = "";
+            document.getElementById("Sisältöteksti").innerHTML = "";
+            
+            // näyttää ajankohtaisen luukun kun painettu
+            document.getElementById("btn1").style.backgroundColor = "green";
             }else{
-            window.alert("Ei toimi!")
+            window.alert("Väärä viikko Donkey!")
             }
         }
         function kalenteriLuukku11(){
             if(paiva >= 29 && kuukausi >= 7){
-            document.getElementById("teksti").innerHTML = "Hyvä";
+            // Poista aiempi kuva-alueen sisältö
+            document.getElementById("kuva-alue").innerHTML = "";
+        
+            // Luo uusi kuvaelementti
+
+            uusiKuva.src = ""; // Aseta uuden kuvan lähde
+    
+            // Lisää kuva-alueeseen
+            document.getElementById("kuva-alue").appendChild(uusiKuva);
+    
+            //otsikko- ja sisältötekstit
+            document.getElementById("Otsikkoteksti").innerHTML = "";
+            document.getElementById("Sisältöteksti").innerHTML = "";
+            
+            // näyttää ajankohtaisen luukun kun painettu
+            document.getElementById("btn1").style.backgroundColor = "green";
             }else{
-            window.alert("Ei toimi!")
+            window.alert("Väärä viikko Donkey!")
             }
         }
         function kalenteriLuukku12(){
             if(paiva >= 5 && kuukausi >= 8){
-            document.getElementById("teksti").innerHTML = "Hyvä";
+            // Poista aiempi kuva-alueen sisältö
+            document.getElementById("kuva-alue").innerHTML = "";
+        
+            // Luo uusi kuvaelementti
+
+            uusiKuva.src = ""; // Aseta uuden kuvan lähde
+    
+            // Lisää kuva-alueeseen
+            document.getElementById("kuva-alue").appendChild(uusiKuva);
+    
+            //otsikko- ja sisältötekstit
+            document.getElementById("Otsikkoteksti").innerHTML = "";
+            document.getElementById("Sisältöteksti").innerHTML = "";
+            
+            // näyttää ajankohtaisen luukun kun painettu
+            document.getElementById("btn1").style.backgroundColor = "green";
             }else{
-            window.alert("Ei toimi!")
+            window.alert("Väärä viikko Donkey!")
             }
         }
         function kalenteriLuukku13(){
             if(paiva >= 12 && kuukausi >= 8){
-            document.getElementById("teksti").innerHTML = "Hyvä";
+            // Poista aiempi kuva-alueen sisältö
+            document.getElementById("kuva-alue").innerHTML = "";
+        
+            // Luo uusi kuvaelementti
+
+            uusiKuva.src = ""; // Aseta uuden kuvan lähde
+    
+            // Lisää kuva-alueeseen
+            document.getElementById("kuva-alue").appendChild(uusiKuva);
+    
+            //otsikko- ja sisältötekstit
+            document.getElementById("Otsikkoteksti").innerHTML = "";
+            document.getElementById("Sisältöteksti").innerHTML = "";
+            
+            // näyttää ajankohtaisen luukun kun painettu
+            document.getElementById("btn1").style.backgroundColor = "green";
             }else{
-            window.alert("Ei toimi!")
+            window.alert("Väärä viikko Donkey!")
             }
         }
